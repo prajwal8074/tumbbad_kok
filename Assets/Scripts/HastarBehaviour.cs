876d32f90c610f35069141fbc140f669b605905a
prajwal8074 <prajwal8074@gmail.com>
2025-03-01 14:44:06 +0530

    added dummy run animation

---
+++ Assets/Scripts/HastarBehaviour.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class HastarBehaviour : MonoBehaviour
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else{
+            animator.SetBool("isRunning", false);
+        }
+    }
+}
